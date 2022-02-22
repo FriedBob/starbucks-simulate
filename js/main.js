8373c55b23e6d969d9a990d5b0520728f207ed31
isshin123 <isshin95@gmail.com>
2022-02-22 15:53:24 +0900

    전역 배지

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+/*--- USED OPEN-SOURCE LIBRARY ---*/
+// Lodash : 함수의 ms단위의 연속실행 제약
+// GSAP : js의 애니메이션 처리
+
+
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
@@ -15,3 +20,34 @@ searchInputEl.addEventListener('blur', function(){
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder','');
 });
+
+/*--- badge scroll control ---*/
+const badgeEl = document.querySelector('header .badges');
+
+// window = browser window
+// Lodash script를 이용하여 fucntion의 ms단위 연속실행 제약
+// ._throttle(fucntion(){}, ms)
+// GSAP 을 이용한 js 애니메이션 처리
+// gasp.to(요소, 지속시간(초), 옵션(객체))
+
+/* opacity속성처럼 숫자값들은 전환효과(css transition, js GASP)를 통해 요소의 전/후 상태를 중간숫자의 값으로
+   자연스럽게 만들어 줄 수 있지만, display 속성처럼 값이 숫자가 아닌 속성들은 
+   전/후 상태의 중간값이 존재하지 않기 때문에, 자연스러운 전환 효과를 적용할 수 없다 */
+window.addEventListener('scroll', _.throttle(function(){
+    console.log(window.scrollY);
+    if(window.scrollY > 500){
+        // badge hide
+        // badgeEl.style.display = 'none';
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+    }else{
+        // badge visible
+        // badgeEl.style.display = 'block';
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+}, 300));
